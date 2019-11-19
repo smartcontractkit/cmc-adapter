0f41c79239aa66df2af0bbaa03d1db30a1fa172b
Thomas Hodges <me@thodges.com>
2019-11-19 10:36:54 -0600

    Update adapter to use result key

--- index.js
+++ index.js
@@ -1,73 +1,75 @@
-let request = require('request');
+let request = require('request')
 
 const createRequest = (input, callback) => {
-	const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
-	const sym = input.data.sym || input.data.coin || "";
-	const cid = input.data.cid || ""; // CMC allows a coin ID to be specified instead of a symbol
-	// Free CMCPro API only supports a single symbol to convert
-	let convert = input.data.convert || input.data.market || "";
-	let requestObj;
-	if (sym.length > 0) {
-		requestObj = {
-			symbol: sym,
-			convert: convert
-		}
-	} else {
-		requestObj = {
-			id: cid,
-			convert: convert
-		}
-	}
-	let headerObj = {
-		"X-CMC_PRO_API_KEY": process.env.API_KEY
-	};
-	let options = {
-		url: url,
-		headers: headerObj,
-		qs: requestObj,
-		json: true
-	};
-	request(options, (error, response, body) => {
-		if (error || response.statusCode >= 400) {
-			callback(response.statusCode, {
-				jobRunID: input.id,
-				status: "errored",
-				error: body,
-				errorMessage: body.status.error_message,
-				statusCode: response.statusCode
-			});
-		} else {
-			callback(response.statusCode, {
-				jobRunID: input.id,
-				data: body,
-				// Only return the price of the first symbol if multiple are specified
-				result: body.data[sym].quote[convert].price,
-				statusCode: response.statusCode
-			});
-		}
-	});
-};
+  const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+  const sym = input.data.sym || input.data.coin || ''
+  const cid = input.data.cid || '' // CMC allows a coin ID to be specified instead of a symbol
+  // Free CMCPro API only supports a single symbol to convert
+  let convert = input.data.convert || input.data.market || ''
+  let requestObj
+  if (sym.length > 0) {
+    requestObj = {
+      symbol: sym,
+      convert: convert
+    }
+  } else {
+    requestObj = {
+      id: cid,
+      convert: convert
+    }
+  }
+  let headerObj = {
+    'X-CMC_PRO_API_KEY': process.env.API_KEY
+  }
+  let options = {
+    url: url,
+    headers: headerObj,
+    qs: requestObj,
+    json: true
+  }
+  request(options, (error, response, body) => {
+    if (error || response.statusCode >= 400) {
+      callback(response.statusCode, {
+        jobRunID: input.id,
+        status: 'errored',
+        error: body,
+        errorMessage: body.status.error_message,
+        statusCode: response.statusCode
+      })
+    } else {
+      // Only return the price of the first symbol if multiple are specified
+      const result = body.data[sym].quote[convert].price
+      body.result = result
+      callback(response.statusCode, {
+        jobRunID: input.id,
+        data: body,
+        result: result,
+        statusCode: response.statusCode
+      })
+    }
+  })
+}
 
 exports.gcpservice = (req, res) => {
-	createRequest(req.body, (statusCode, data) => {
-		res.status(statusCode).send(data);
-	});
-};
+  createRequest(req.body, (statusCode, data) => {
+    res.status(statusCode).send(data)
+  })
+}
 
 exports.handler = (event, context, callback) => {
-	createRequest(event, (statusCode, data) => {
-		callback(null, data);
-	});
-};
+  createRequest(event, (statusCode, data) => {
+    callback(null, data)
+  })
+}
 
 exports.handlerv2 = (event, context, callback) => {
-	createRequest(JSON.parse(event.body), (statusCode, data) => {
-		callback(null, {
-			statusCode: statusCode,
-			body: JSON.stringify(data),
-			isBase64Encoded: false
-		});
-	});
-};
+  createRequest(JSON.parse(event.body), (statusCode, data) => {
+    callback(null, {
+      statusCode: statusCode,
+      body: JSON.stringify(data),
+      isBase64Encoded: false
+    })
+  })
+}
 
-module.exports.createRequest = createRequest;
+module.exports.createRequest = createRequest
--- test/index_test.js
+++ test/index_test.js
@@ -1,68 +1,70 @@
-const assert = require('chai').assert;
-const createRequest = require('../index.js').createRequest;
+const assert = require('chai').assert
+const createRequest = require('../index.js').createRequest
 
 
 describe('createRequest', () => {
-	context('requesting sym and convert', () => {
-		const jobID = "278c97ffadb54a5bbb93cfec5f7b5503";
-		const req = {
-			id: jobID,
-			data: {
-				sym: "ETH",
-				convert: "USD"
-			}
-		};
+  context('requesting sym and convert', () => {
+    const jobID = '278c97ffadb54a5bbb93cfec5f7b5503'
+    const req = {
+      id: jobID,
+      data: {
+        sym: 'ETH',
+        convert: 'USD'
+      }
+    }
 
-		it('returns data to the node', (done) => {
-			createRequest(req, (statusCode, data) => {
-				assert.equal(statusCode, 200);
-				assert.equal(data.jobRunID, jobID);
-				assert.isNotEmpty(data.data);
-				assert.isNumber(data.result);
-				done();
-			});
-		});
-	});
+    it('returns data to the node', (done) => {
+      createRequest(req, (statusCode, data) => {
+        assert.equal(statusCode, 200)
+        assert.equal(data.jobRunID, jobID)
+        assert.isNotEmpty(data.data)
+        assert.isNumber(data.data.result)
+        assert.isNumber(data.result)
+        done()
+      })
+    })
+  })
 
-	context('requesting coin and market', () => {
-		const jobID = "278c97ffadb54a5bbb93cfec5f7b5506";
-		const req = {
-			id: jobID,
-			data: {
-				coin: "ETH",
-				market: "USD"
-			}
-		};
+  context('requesting coin and market', () => {
+    const jobID = '278c97ffadb54a5bbb93cfec5f7b5506'
+    const req = {
+      id: jobID,
+      data: {
+        coin: 'ETH',
+        market: 'USD'
+      }
+    }
 
-		it('returns data to the node', (done) => {
-			createRequest(req, (statusCode, data) => {
-				assert.equal(statusCode, 200);
-				assert.equal(data.jobRunID, jobID);
-				assert.isNotEmpty(data.data);
-				assert.isNumber(data.result);
-				done();
-			});
-		});
-	});
+    it('returns data to the node', (done) => {
+      createRequest(req, (statusCode, data) => {
+        assert.equal(statusCode, 200)
+        assert.equal(data.jobRunID, jobID)
+        assert.isNotEmpty(data.data)
+        assert.isNumber(data.data.result)
+        assert.isNumber(data.result)
+        done()
+      })
+    })
+  })
 
-	context('with a bad request', () => {
-		const jobID = "278c97ffadb54a5bbb93cfec5f7b5506";
-		const req = {
-			id: jobID,
-			data: {
-				coin: "notreal",
-				market: "notreal"
-			}
-		};
+  context('with a bad request', () => {
+    const jobID = '278c97ffadb54a5bbb93cfec5f7b5506'
+    const req = {
+      id: jobID,
+      data: {
+        coin: 'notreal',
+        market: 'notreal'
+      }
+    }
 
-		it('returns an error to the node', (done) => {
-			createRequest(req, (statusCode, data) => {
-				assert.isAtLeast(statusCode, 400);
-				assert.equal(data.jobRunID, jobID);
-				assert.isNotEmpty(data.error);
-				assert.isNotEmpty(data.errorMessage);
-				done();
-			});
-		});
-	});
-});
+    it('returns an error to the node', (done) => {
+      createRequest(req, (statusCode, data) => {
+        assert.isAtLeast(statusCode, 400)
+        assert.equal(data.jobRunID, jobID)
+        assert.isNotEmpty(data.error)
+        assert.isNotEmpty(data.errorMessage)
+        done()
+      })
+    })
+  })
+})
